c01eae620ae713200e4b770a591a78143ec01ac4
ZaryabSoomro <zaryabsoomro1@outlook.com>
2025-08-06 14:31:03 +0500

    90% done

---
+++ script.js
@@ -0,0 +1,11 @@
+function showmenu(){
+    document.getElementById("open").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("close").style.display = "block";
+}
+function hidemenu(){
+    document.getElementById("close").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("open").style.display = "block";
+
+}
